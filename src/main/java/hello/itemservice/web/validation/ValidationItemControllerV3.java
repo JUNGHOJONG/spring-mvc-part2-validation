4ac984ebdeac4f6d545039a43cc9c2ede1e62a55
JUNGHOJONG <hojong1351@naver.com>
2021-09-24 14:33:21 +0900

    Bean Validation - 수정에 적용

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -78,6 +78,14 @@ public class ValidationItemControllerV3 {
     @PostMapping("/{itemId}/edit")
     public String editItem(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult, Model model) {
 
+        //글로벌 오류 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
         if (bindingResult.hasErrors()) {
             log.info("bindingResult={}", bindingResult);
             return "/validation/v3/editForm";
